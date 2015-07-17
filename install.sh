7e3c2cd7ba9b633f3598a62fcf651c7c7fc7b2fb
guanglinlv <guanglinlv@gmail.com>
2015-07-17 17:21:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,8 +10,19 @@ cd $WORK_DIR
 
 if [ ! -x $SRC_DIR/make.sh ]; then chmod a+x $SRC_DIR/make.sh ; fi
 $SRC_DIR/make.sh
+if [ $? != 0 ]; then echo "Invoke $SRC_DIR/make.sh failed,check abover error!" ; exit 1 ; fi
 
-if [ -f $SRC_DIR/nsattach ]; then /bin/bash -c "\cp -f $SRC_DIR/nsattach $TARGET_DIR" ; fi
-if [ -f $SRC_DIR/container-enter ]; then /bin/bash -c "\cp -f $SRC_DIR/container-enter $TARGET_DIR" ; fi
+if [ -f $SRC_DIR/nsattach ]; then 
+	/bin/bash -c "\cp -f $SRC_DIR/nsattach $TARGET_DIR"
+	echo "$(\ls -l $TARGET_DIR/nsattach)"
+fi
+if [ -f $SRC_DIR/container-enter ]; then 
+	/bin/bash -c "\cp -f $SRC_DIR/container-enter $TARGET_DIR"
+	echo "$(\ls -l $TARGET_DIR/container-enter)" 
+fi
+if [ -f $SRC_DIR/container-mount-volumes ]; then 
+	/bin/bash -c "\cp -f $SRC_DIR/container-mount-volumes $TARGET_DIR" 
+	echo "$(\ls -l $TARGET_DIR/container-mount-volumes)" 
+fi
 
 
